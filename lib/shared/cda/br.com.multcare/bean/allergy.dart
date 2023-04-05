4c473fe771509b34f37f1a8299e32f64d2a0a2a8
Jacques Wolbeck <jacqueswolbeck@gmail.com>
2023-04-05 00:03:32 -0300

    initial cda implementation

---
+++ lib/shared/cda/br.com.multcare/bean/allergy.dart
@@ -0,0 +1,7 @@
+class Allergy {
+  List<String>? _content;
+  Allergy();
+
+  void setContent(List<String> content) => _content = content;
+  List<String>? getContent() => _content;
+}
---
+++ lib/shared/cda/br.com.multcare/bean/related.dart
@@ -0,0 +1,24 @@
+class Related {
+  String? _code;
+  String? _id;
+  String? _extension;
+  String? _version;
+
+  Related();
+
+  void setCode(String code) => _code = code;
+
+  String? getCode() => _code;
+
+  void setID(String id) => _id = id;
+
+  String? getId() => _id;
+
+  void setExtension(String extenSion) => _extension = extenSion;
+
+  String? getExtension() => _extension;
+
+  void setVersion(String version) => _version = version;
+
+  String? getVersion() => _version;
+}
